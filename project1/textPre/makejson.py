697a1cb2d07800ecb3ef9357a08374daeb74aefa
yjlee <YJbonobono>
2021-05-08 23:20:26 -0700

    modified-ubuntu

--- project1/textPre/makejson.py
+++ project1/textPre/makejson.py
@@ -5,6 +5,7 @@ def makejson(url, wordlist):
     config = OrderedDict()
     config["languageCode"] = "ko-KR"
     config["speechContexts"] = [{"phrases" : wordlist}]
-    with open(url + 'file.json', 'w', encoding='utf-8') as make_file:
+    with open(url + 'PDFPPT_dataset.json', 'w', encoding='utf-8') as make_file:
         json.dump(config, make_file, ensure_ascii=False, indent='\t')
     print(".json created")
+
